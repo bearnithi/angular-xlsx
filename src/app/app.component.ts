c00b558ef3ccff930c902a5d6c40582002952608
Nithi <58935041+imNithi@users.noreply.github.com>
2022-10-25 23:42:10 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'excel-test';
+  
+  onFileChange(event: any) {
+    const target: DataTransfer = <DataTransfer>(event.target);
+    if (target.files.length !== 1) {
+      throw new Error('Cannot use multiple files');
+    }
+    const reader: FileReader = new FileReader();
+    reader.readAsBinaryString(target.files[0]);
+    reader.onload = (e: any) => {
+      const binarystr: string = e.target.result;
+      const wb: any = XLSX.read(binarystr, { type: 'binary' });
+      const hiddenSheets = wb.Workbook?.Sheets?.find((sheet: any) => sheet.Hidden === 1)
+      console.log(hiddenSheets, "HIDDEN SHEETS");
+    };
+ }
 }
